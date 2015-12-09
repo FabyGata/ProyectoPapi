650d1680c3a1db430eec008d1c35f65a669d4277
Fabiola Vanessa Aliaga Salvatierra <fabita.aliaga@gmail.com>
2015-12-09 13:59:48 -0400

    El diseño del encabezado :)

--- resources/views/caja.blade.php
+++ resources/views/caja.blade.php
@@ -9,17 +9,19 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Justified Nav Template for Bootstrap</title>
+    <title>Caja</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
 
     <!-- Custom styles for this template -->
-    <link href="justified-nav.css" rel="stylesheet">
+    <link href="{{asset('vendor/css/justified-nav.css')}}" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <script src="{{asset('vendor/bootstrap/assets/js/ie-emulation-modes-warning.js')}}"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -35,27 +37,30 @@
     <!-- The justified navigation menu is meant for single line per list item.
          Multiple lines will require custom code not provided by Bootstrap. -->
     <div class="masthead">
-        <h3 class="text-muted">Project name</h3>
+        <h3 class="text-muted">Project name :v </h3>
         <nav>
             <ul class="nav nav-justified">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Downloads</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
+                <li class="active"><a href="#">Caja</a></li>
+                <li><a href="#">Ventas</a></li>
+                <li><a href="#">Configuraciones</a></li>
+
             </ul>
         </nav>
     </div>
 
     <!-- Jumbotron -->
+    <!--
     <div class="jumbotron">
         <h1>Marketing stuff!</h1>
         <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
         <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
     </div>
+    -->
+
 
     <!-- Example row of columns -->
+
+    <!--
     <div class="row">
         <div class="col-lg-4">
             <h2>Safari bug warning!</h2>
@@ -74,16 +79,18 @@
             <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
         </div>
     </div>
+    -->
 
-    <!-- Site footer -->
+    <!-- Site footer
     <footer class="footer">
         <p>&copy; Company 2014</p>
     </footer>
+    -->
 
 </div> <!-- /container -->
 
 
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+<script src="{{asset('vendor/bootstrap/assets/js/ie10-viewport-bug-workaround.js')}}"></script>
 </body>
 </html>
